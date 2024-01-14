bf0748a768b87c3192c29699ac8c34147d20aaae
Slavica <slavicanikolik1@gmail.com>
2024-01-14 17:50:04 +0100

    Add project files.

---
+++ Source/Pages/LoginPage.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObjectModel.Source.Pages
+{
+    public class LoginPage
+    {
+
+        [FindsBy(How = How.Id, Using = "user-name")]
+        private IWebElement usernameField;
+
+
+        [FindsBy(How = How.Id, Using = "password")]
+        private IWebElement passwordField;
+
+
+        [FindsBy(How = How.Id, Using = "login-button")]
+        private IWebElement submitLoginForm;
+
+        private IWebDriver _driver;
+
+        public LoginPage(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void login(string username, string password)
+        {
+            usernameField.SendKeys(username);
+            passwordField.SendKeys(password);
+            submitLoginForm.Click();
+        }
+
+    }
+}
---
+++ Test/LoginTest.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using PageObjectModel.Source.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace PageObjectModel.Test
+{
+    public class LoginTest
+    {
+
+        private IWebDriver driver;
+
+        [SetUp]
+        public void setUp()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void loginTest() {
+        
+            LoginPage loginPage = new LoginPage(driver);
+            driver.Navigate().GoToUrl("https://www.saucedemo.com");
+            loginPage.login("standard_user", "secret_sauce");
+            Assert.True(driver.Title.Contains("Swag Labs"));
+        }
+
+        [TearDown] public void tearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
